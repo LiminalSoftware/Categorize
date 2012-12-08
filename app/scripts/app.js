1bbcecfa3f13e00ae84c77d14840ccf985756cf6
Nembus <timurlan77@gmail.com>
2012-12-08 05:21:59 -0500

    Basic layout of the two pages.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,15 @@
-var categorizeApp = angular.module('categorizeApp', []);
+var categorizeApp = angular.module('categorizeApp', [])
+  .config(function($routeProvider) {
+
+    $routeProvider.
+      when('/', {
+        controller: 'IndexController',
+        templateUrl: 'views/main.html'
+      }).
+      when('/mods/:modId', {
+        controller: 'ModController',
+        templateUrl: 'views/mod-detail.html'
+      });
+  });
 
 
