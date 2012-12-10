2dbf35ee0faa1cbf09c247922ebaf4ac576f5ce8
Nembus <timurlan77@gmail.com>
2012-12-10 04:30:58 -0500

    Got the mod service to run.

--- app/js/app.js
+++ app/js/app.js
@@ -1,66 +1,66 @@
 var categorizeApp = angular.module('categorizeApp', ['ngResource'])
-    .config(function ($routeProvider) {
+  .config(function($routeProvider) {
 
-      $routeProvider.
-        when('/', {
-          controller: 'IndexController',
-          templateUrl: 'views/main.html'
-        }).
-        when('/mods/:modId', {
-          controller: 'ModController',
-          templateUrl: 'views/mod-detail.html'
+    $routeProvider.
+      when('/', {
+        controller: 'IndexController',
+        templateUrl: 'views/main.html'
+      }).
+      when('/mods/:modId', {
+        controller: 'ModController',
+        templateUrl: 'views/mod-detail.html'
+      });
+  })
+
+  .factory('categoryResource', ['$resource',
+    function($resource) {
+      return $resource('http://wiglepedia.org/categories', {callback: 'JSON_CALLBACK'}, {
+        get: {method: 'JSONP', isArray: true}
+      });
+    }])
+
+//    .factory('modResource', ['$resource',
+//      function($resource) {
+//        return $resource('http://wiglepedia.org/mods/count/100', {callback: 'JSON_CALLBACK'}, {
+//          get: {method: 'JSONP', isArray: true}
+//        });
+//      }])
+
+  .controller('IndexController', function IndexController($scope, modService) {
+    $scope.mods = modService.getAllMods();
+  })
+
+  .controller('ModController', function ModController($scope, $routeParams, modService, categoryResource) {
+    $scope.currentMod = modService.getCurrentMod($routeParams.modId);
+    $scope.allCategories = categoryResource.get();
+  })
+
+  .service('modService', ['$resource', function($resource) {
+    'use strict';
+    var currentMod = {};
+    var allMods = [];
+    var res = $resource('http://wiglepedia.org/mods/count/100', {callback: 'JSON_CALLBACK'}, {
+      get: {method: 'JSONP', isArray: true}
+    });
+
+    return {
+      resource: res,
+      getCurrentMod: function(id) {
+        var result = allMods.filter(function(mod) {
+          return mod.id == id;
         });
-    })
-
-    .factory('categoryResource', ['$resource',
-      function ($resource) {
-        return $resource('http://wiglepedia.org/categories', {callback: 'JSON_CALLBACK'}, {
-          get: {method: 'JSONP', isArray: true}
-        });
-      }])
-
-    .factory('modResource', ['$resource',
-      function ($resource) {
-        return $resource('http://wiglepedia.org/mods/count/100', {callback: 'JSON_CALLBACK'}, {
-          get: {method: 'JSONP', isArray: true}
-        });
-      }])
-
-    .controller('IndexController', function IndexController($scope, modService, modResource) {
-      $scope.mods = modResource.get();
-    })
-
-    .controller('ModController', function ($scope, $routeParams, modService, categoryResource) {
-      $scope.currentMod = modService.getCurrentMod($routeParams.modId);
-      $scope.allCategories = categoryResource.get();
-    })
-
-    .service('modService', function () {
-      'use strict';
-      var currentMod = {};
-      //we will get this from the server later...
-      var allMods = [
-        {id: 1, name: 'Red Power 2', category: 'uncategorized', forumLink: 'http://www.redpower2.com'},
-        {id: 2, name: 'BuildCraft', category: 'uncategorized', forumLink: 'http://www.buildcraft.com'},
-        {id: 3, name: 'Portal Gun', category: 'uncategorized', forumLink: 'http://www.portalgun.com'}
-      ];
-      return {
-        getCurrentMod: function (id) {
-          var result = allMods.filter(function (mod) {
-            return mod.id == id;
-          });
-          currentMod = result[0] || currentMod;
-          return currentMod;
-        },
-        setCurrentMod: function (data) {
-          currentMod = data || currentMod;
-        },
-        getAllMods: function () {
-          return allMods;
-        }
+        currentMod = result[0] || currentMod;
+        return currentMod;
+      },
+      setCurrentMod: function(data) {
+        currentMod = data || currentMod;
+      },
+      getAllMods: function() {
+        allMods = this.resource.get();
+        console.log(allMods);
+        return allMods;
       }
-    })
-
-  ;
+    }
+  }]);
 
 
